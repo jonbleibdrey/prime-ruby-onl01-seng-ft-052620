04faabe3deae84d746957d5a36e6f5159772634a
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-06-01 00:46:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,13 @@
 #composite=[1,4,6]
 
 def prime?(num)
+  if num == 0
+    return false
+  elsif num < 0
+    num = -num
+  elsif num ==1
+    return false
+  end
 (2..(num - 1)).each do |n|
     return false if num % n == 0
   end
